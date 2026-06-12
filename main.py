a1754287627c79c4f8f6088e3f43745ee0c34b67
coleman-jarvin <colemanjarvin13@gmail.com>
2026-06-11 23:21:20 -0500

    update...

---
+++ main.py
@@ -0,0 +1,13 @@
+import torch 
+from torch.utils.data import Dataset
+import torch.utils.data.dataloader import DataLoader
+
+from mingpt.model import GPT
+from mingpt.trainer import Trainer
+from mingpt.utils import set_seed, setup_logging, CfgNode as CN
+
+def get_config():
+
+    C = CN()
+    
+    C.system = CN()
